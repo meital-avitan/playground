b32d1ca030437699f423f2f261ce24a9bde36af3
meital <meital.avitan@blazemeter.com>
2019-11-23 15:57:49 +0200

    WAP-12345-a-sample-commit

---
+++ CloudAddresses.php
@@ -0,0 +1,27 @@
+<?php
+    
+namespace blazemeter\Helpers\Cloud;
+
+class CloudAddresses {
+    const OPL_IMAGE_URL = 'https://s3.amazonaws.com/blazemeter-images/v4/1.18.3-1098.tar';
+    const OPL_IMAGE_VERSION = '1.18.3-1098';
+    const AZURE_AMI = 'packer-taurus-1573745237';
+    const GOOGLE_AMI = 'taurus-engine-packer-1573745230';
+    const EC2_AP_NORTHEAST_1_AMI = 'ami-00d594cf6007debeb';
+    const EC2_AP_NORTHEAST_2_AMI = 'ami-0dbaf6500541ac942';
+    const EC2_AP_SOUTH_1_AMI = 'ami-067a17de48902b8d2';
+    const EC2_AP_SOUTHEAST_1_AMI = 'ami-053d86a220f05d100';
+    const EC2_AP_SOUTHEAST_2_AMI = 'ami-0dc83027b097bf911';
+    const EC2_CA_CENTRAL_1_AMI = 'ami-095c7d9a1b35179f4';
+    const EC2_EU_CENTRAL_1_AMI = 'ami-090fadebf8a520ac3';
+    const EC2_EU_WEST_1_AMI = 'ami-0d8a51698e8068c49';
+    const EC2_EU_WEST_2_AMI = 'ami-0b7b15083255c03bc';
+    const EC2_EU_WEST_3_AMI = 'ami-0c23e291d999b10fd';
+    const EC2_SA_EAST_1_AMI = 'ami-07ae19c43607839aa';
+    const EC2_US_EAST_1_AMI = 'ami-035a56e986a5bb41b';
+    const EC2_US_EAST_2_AMI = 'ami-0851031619e0a40fb';
+    const EC2_US_WEST_1_AMI = 'ami-09010653d04c5bff2';
+    const EC2_US_WEST_2_AMI = 'ami-0b49c040c5b2645b9';
+
+}
+    
